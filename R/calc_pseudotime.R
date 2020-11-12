c06842e41a884baeacaf5d6c8f1875df574d3bde
Anthony Cillo <arc85@pitt.edu>
2020-11-12 00:15:57 -0500

    Create and document pseudotime function

---
+++ R/calc_pseudotime.R
@@ -0,0 +1,24 @@
+#' Calculate pseudotime
+#'
+#' Calculate pseudotemporal order via principal curves for an arbitrary low dimensional embedding for scRNAseq data.
+#'
+#' @param embedding data.frame
+#' @param clusters factor
+#'
+#' @return data.frame
+#' @export
+
+calc_pseudotime <- function(embedding,clusters) {
+
+  row.names <- rownames(embedding)
+
+  #Calculate pseudotime
+  prin.fit <- princurv::principal_curve(embedding,smoother="periodic_lowess")
+
+  #Return data.frame of pseudotime results
+  fit.frame <- data.frame(cell_order=prin.fit$ord,pseudotime=prin.fit$lambda)
+  rownames(fit.frame) <- row.names
+
+  return(fit.frame)
+
+}
