9c9e7e488d940562bd43e8dec285ba2d303469ed
Anthony Cillo <arc85@pitt.edu>
2020-11-12 10:39:12 -0500

    Include embedding means by cluster in order_clusters_by_time

--- R/order_clusters_by_time.R
+++ R/order_clusters_by_time.R
@@ -22,6 +22,24 @@ order_clusters_by_time <- function(embedding,clusters,pseudotime) {
   mean.pseudotime.cluster <- mean.pseudotime.cluster %>%
     dplyr::arrange(mean_lambda) %>% dplyr::mutate(new_cluster_id=seq_along(1:nrow(mean.pseudotime.cluster)))
 
-  return(mean.pseudotime.cluster)
+  #Calculate embedding means of each cluster
+  embeds.calc.mean <- grep("clusters|pseudotime",colnames(fit.frame),invert=TRUE,value=TRUE)
+
+  cluster.split <- split(fit.frame,fit.frame$clusters)
+
+  cluster.means <- t(sapply(cluster.split,function(x) {
+    apply(x[,embeds.calc.mean],2,mean)
+  }))
+
+  colnames(cluster.means) <- paste(colnames(cluster.means),"mean",sep="_")
+
+  cluster.means <- tibble::as_tibble(cluster.means,rownames="clusters")
+  cluster.means$clusters <- as.factor(cluster.means$clusters)
+
+  mean.pseudotime.cluster <- dplyr::left_join(mean.pseudotime.cluster,cluster.means,by="clusters")
+
+  mean.pseudotime.cluster <- rbind(mean.pseudotime.cluster,mean.pseudotime.cluster[1,])
+
+  return(data.frame(mean.pseudotime.cluster))
 
 }
