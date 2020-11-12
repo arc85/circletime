651d18ab9b3885398592a24e3439b9f8352236dc
Anthony Cillo <arc85@pitt.edu>
2020-11-12 00:32:49 -0500

    Update order_cluster_by_time

--- R/order_cluster_by_time.R
+++ R/order_cluster_by_time.R
@@ -1,15 +1,18 @@
 #' Order clusters temporally
 #'
-#' Derive new cluster identities by progression through pseudotime
+#' Derive new cluster identities by progression through pseudotime. This function returns a new data.frame with clusters ordered by pseudotemporal progression.
 #'
 #' @param embedding data.frame
 #' @param clusters factor
 #' @param pseudotime numeric
 #'
+#' @importFrom magittr "%>%"
 #' @return data.frame
 #' @export
 
 order_clusters_by_time <- function(embedding,clusters,pseudotime) {
+
+  fit.frame <- data.frame(embedding,cluster,pseudotime)
 
   #Reorder clusters by progression through pseudotime
   mean.pseudotime.cluster <- fit.frame %>%
@@ -19,12 +22,6 @@ order_clusters_by_time <- function(embedding,clusters,pseudotime) {
   mean.pseudotime.cluster <- mean.pseudotime.cluster %>%
     arrange(mean_lambda) %>% mutate(new_cluster_id=seq_along(1:nrow(mean.pseudotime.cluster)))
 
-  level.key <- c(rd3.means$new_cluster_id)
-  names(level.key) <- rd3.means$clusters
-  pca.plot2 <- pca.plot %>% mutate(new_clusters=recode(clusters,!!!level.key))
-
-  cluster.means <- left_join(cluster.means,rd3.means,by="clusters")
-  cluster.means <- cluster.means %>% arrange(new_cluster_id)
-  cluster.means <- rbind(cluster.means,cluster.means[1,])
+  return(mean.pseudotime.cluster)
 
 }
