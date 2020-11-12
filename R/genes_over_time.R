30a8150d30db1139483ee75bee25be4e10f3f812
Anthony Cillo <arc85@pitt.edu>
2020-11-12 00:50:22 -0500

    Create and document genes over time function

---
+++ R/genes_over_time.R
@@ -0,0 +1,32 @@
+#' Fit genes as a function of pseudotime
+#'
+#' Utilize generalized additive models to fit gene expression as a function of pseudotime.
+#'
+#' @param expr.matrix data.frame
+#' @param pseudotime numeric
+#'
+#' @return list
+#' @export
+
+genes_over_time <- function(expr.matrix,pseudotime) {
+
+expr.list <- split(expr.matrix,rownames(expr.matrix))
+t <- pseudotime
+
+p.val <- lapply(expr.list,function(z) {
+  d <- data.frame(z=z,t=t)
+  tmp <- gam::gam(z~gam::lo(t),data=d)
+  p <- summary(tmp)[4][[1]][1,5]
+})
+
+fits <- lapply(Y.list,function(z) {
+  d <- data.frame(z=z,t=t)
+  tmp <- gam::gam(z~gam::lo(t),data=d)
+  predict(tmp,data=d)
+})
+
+names(fits) <- names(p.val) <- rownames(expr.matrix)
+
+list(p_values=p.val,expression_fits=fits)
+
+}
