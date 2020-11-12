73c2b33b81a526014fa2bcd9054976db78d51025
Anthony Cillo <arc85@pitt.edu>
2020-11-12 08:55:32 -0500

    Include minimum cell cycle embedding data

---
+++ R/cycle_small.R
@@ -0,0 +1,12 @@
+#' Cycle embeddings
+#'
+#' This dataset is a 2 dimensional PC embedding of cells progressing through the cell cycle, with some clusters assigned.
+#'
+#' @format A data frame with 774 rows and 51 columns:
+#' \describe{
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   ...
+#' }
+#' @source \url{https://pubmed.ncbi.nlm.nih.gov/27365425/}
+"cycle.small"
